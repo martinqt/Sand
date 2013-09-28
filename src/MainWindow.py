dbdbe688b23e85ed503811b616a24cf10b7341ec
martinqt <m.ki2@laposte.net>
2013-09-28 11:48:46 +0200

    Add collapse and collapseAll

--- src/MainWindow.py
+++ src/MainWindow.py
@@ -20,12 +20,15 @@ class MainWindow(QMainWindow):
 
         self.collapseButton = QPushButton('Collapse', self)
         self.collapseButton.clicked.connect(self.collapse)
+        self.collapseAllButton = QPushButton('Collapse All', self)
+        self.collapseAllButton.clicked.connect(self.collapseAll)
 
         widget = QWidget(self)
         layout = QVBoxLayout()
 
         layout.addWidget(self.tableView)
         layout.addWidget(self.collapseButton)
+        layout.addWidget(self.collapseAllButton)
 
         widget.setLayout(layout)
         self.setCentralWidget(widget)
@@ -34,10 +37,17 @@ class MainWindow(QMainWindow):
 
     def populate(self):
         self.table.setItem(0, 5, QStandardItem('4'))
+        self.table.setItem(0, 4, QStandardItem('3'))
         self.table.setItem(2, 3, QStandardItem('3'))
         self.table.setItem(2, 1, QStandardItem('2'))
 
     def collapse(self):
+        """Step by step collapse."""
+        for cell in self.table.collapsable():
+            self.table.collapse(cell[0], cell[1])
+
+    def collapseAll(self):
+        """Collapse all at once."""
         while self.table.collapsable() != []:
             for cell in self.table.collapsable():
                 self.table.collapse(cell[0], cell[1])
