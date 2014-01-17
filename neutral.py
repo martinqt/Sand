da0b4d2d1c4ea881275819a847a8738f3135b0e0
martinqt <m.ki2@laposte.net>
2014-01-17 18:11:17 +0100

    Rework neutral to allow restore
    Still WIP

--- neutral.py
+++ neutral.py
@@ -4,73 +4,96 @@ import png, sys, time
 from src.Table import Table
 from PySide.QtGui import QStandardItem
 
-print('Table size:')
-size = int(input())
-
 zoom = 1
-
 colors = list()
 colors.append([5, 205, 0]*zoom)
 colors.append([255, 240, 94]*zoom)
 colors.append([249, 164, 0]*zoom)
 colors.append([210, 59, 0]*zoom)
 
+def createInitialTable(size):
+    table = Table(size, size, 4)
+    table.fill('6')
 
-table = Table(size, size, 4)
-table.fill('6')
+    return table
 
-t1 = time.clock()
-print('Start Initial Collapse')
-sys.stdout.flush()
+def collapseTable(table):
+    t1 = time.clock()
+    print('Start Table Collapse')
+    sys.stdout.flush()
 
-table.collapseAll()
+    table.collapseAll()
 
-t2 = time.clock()
-print('End Initial Collapse '+str(t2-t1))
-sys.stdout.flush()
+    t2 = time.clock()
+    print('End Table Collapse '+str(t2-t1))
+    sys.stdout.flush()
 
-neutral = Table(size, size, 4)
+    return t2
 
-i = 0
+def neutralTable(table, size, t2):
+    neutral = Table(size, size, 4)
+    i = 0
 
-while i < size:
-    j = 0
-    while j < size:
-        neutral.setItem(i, j, QStandardItem(str(3+3-int(table.item(i, j).text()))))
-        j += 1
-    i += 1
+    while i < size:
+        j = 0
+        while j < size:
+            neutral.setItem(i, j, QStandardItem(str(3+3-int(table.item(i, j).text()))))
+            j += 1
+        i += 1
 
-i = j = 0
+    t3 = time.clock()
+    print('End Neutral Generation '+str(t3-t2))
+    sys.stdout.flush()
 
-t3 = time.clock()
-print('End Neutral Generation '+str(t3-t2))
-sys.stdout.flush()
+    return neutral
 
-neutral.collapseAll()
+def generateImage(neutral, size, t4, t0):
+    mat = list()
+    i = 0
 
-t4 = time.clock()
-print('End Neutral Collapse '+str(t4-t3))
-sys.stdout.flush()
+    while i < size:
+        j = 0
+        row = list()
+        while j < size:
+            row.append(colors[int(neutral.item(i, j).text())])
+            j +=1
+        merged = [item for sublist in row for item in sublist]
+        for a in range(zoom):
+            mat.append(merged)
+        i += 1
 
-mat = list()
+    print('End Image Loop '+str(time.clock()-t4))
+    sys.stdout.flush()
 
-while i < size:
-    row = list()
-    while j < size:
-        row.append(colors[int(neutral.item(i, j).text())])
-        j +=1
-    merged = [item for sublist in row for item in sublist]
-    for a in range(zoom):
-        mat.append(merged)
-    j = 0
-    i += 1
+    f = open('pics/'+str(size)+'.png', 'wb')
+    w = png.Writer(size*zoom, size*zoom)
+    w.write(f, mat) 
+    f.close()
 
-print('End Image Loop '+str(time.clock()-t4))
-sys.stdout.flush()
+    print('== Ellapsed Time: '+str(time.clock()-t0)+' ==')
 
-f = open('pics/'+str(size)+'.png', 'wb')
-w = png.Writer(size*zoom, size*zoom)
-w.write(f, mat) 
-f.close()
 
-print('== Ellapsed Time: '+str(time.clock()-t1)+' ==')
+
+#======   MAIN PART    =========================================
+try:
+    cont = sys.argv[1]
+except IndexError:
+    cont = ''
+if cont == '--restore':
+    print('== Restoring Previous Collapsing ==')
+    print('Restore Initial Collapse?')
+    ans = input()
+    if(ans=='y' or ans=='1'):
+        print('from initial')
+    else:
+        print('from neutral')
+else:
+    print('Table size:')
+    size = int(input())
+
+    t0 = time.clock()
+    table = createInitialTable(size)
+    t2 = collapseTable(table)
+    neutral = neutralTable(table, size, t2)
+    t4 = collapseTable(neutral)
+    generateImage(neutral, size, t4, t0)
